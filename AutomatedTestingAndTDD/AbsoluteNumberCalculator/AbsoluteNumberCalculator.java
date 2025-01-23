f2aa93aafd6eb0fc8dc94923f22911567ad540f7
Trung <hotrung1548@gmail.com>
2025-01-23 15:13:13 +0700

    [Thực hành] Tìm giá trị tuyệt đối

---
+++ AutomatedTestingAndTDD/AbsoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,7 @@
+package AutomatedTestingAndTDD.AbsoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        return number > 0 ? number : -number;
+    }
+}
