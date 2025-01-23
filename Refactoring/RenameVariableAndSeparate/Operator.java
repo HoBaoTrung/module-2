efc89489608c787fd17f3b4732bd462be3168905
Trung <hotrung1548@gmail.com>
2025-01-23 14:37:58 +0700

    [Thực hành] Ứng dụng máy tính đơn giản

--- Refactoring/RenameVariableAndSeparate/Operator.java
+++
@@ -1,8 +0,0 @@
-package Refactoring.RenameVariableAndSeparate;
-
-public interface Operator {
-    char ADDITION = '+';
-    char SUBTRACTION = '-';
-    char MULTIPLICATION = '*';
-    char DIVISION = '/';
-}
