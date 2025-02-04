6a9cc0c60c35940cf43f00718b872d102e05770d
Trung <hotrung1548@gmail.com>
2025-02-04 19:52:15 +0700

    [Bài tập] Đếm số lần xuất hiện của mỗi từ trong một chuỗi sử dụng Map

--- StackAndQueue/CountCharWithTreeMap/Main.java
+++ StackAndQueue/CountCharWithTreeMap/Main.java
@@ -1,18 +1,25 @@
 package StackAndQueue.CountCharWithTreeMap;
 
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) {
-        String str = "Hello World";
+
+    private static Map<Character, Integer> countCharacter(String str){
         str = str.toUpperCase();
         Map<Character, Integer> charCountMap = new TreeMap<>();
         for(char c : str.toCharArray()) {
             charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
         }
+        return charCountMap;
+    }
 
-        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        for (Map.Entry<Character, Integer> entry : countCharacter(str).entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
