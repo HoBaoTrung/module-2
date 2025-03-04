4b7d98c2e215fdb1214b9b895c6096f712c7a4a1
Trung <hotrung1548@gmail.com>
2025-03-04 17:32:19 +0700

    [Thực hành] Crawl danh sách bài hát

---
+++ StringAndRegex/CrawlSongExample.java
@@ -0,0 +1,34 @@
+package StringAndRegex;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+public class CrawlSongExample {
+    public static void main(String[] args) {
+        try {
+            // url bị lỗi 403: từ chối quyền truy cập
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // close scanner
+            scanner.close();
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+            // regex
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
